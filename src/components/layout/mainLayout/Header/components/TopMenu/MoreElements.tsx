be35e7c8e9ecb09eaafa30f393e4a727d7a0995a
Davit_Gelovani <65369123+DavitGe@users.noreply.github.com>
2024-06-29 02:52:42 +0400

    #1 - List store and initializing basic component

--- src/components/layout/mainLayout/Header/components/TopMenu/MoreElements.tsx
+++ src/components/layout/mainLayout/Header/components/TopMenu/MoreElements.tsx
@@ -1,13 +1,35 @@
 import React from "react";
 import { IoIosMore } from "react-icons/io";
 import Popover from "../../../../../Popover/Popover";
+import { topMenuElements } from "./store/topMenuElements.conf";
+const List: React.FC<{
+  title: string;
+  elements: { label: string; to: string }[];
+}> = ({ title, elements }) => {
+  return (
+    <div className="flex flex-col">
+      <h4 className="p-0">{title}</h4>
+      <ul className="list-none">
+        {elements.map((el, i) => {
+          return <li key={i}>{el.label}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
 const MoreElements = () => {
   return (
     <Popover
-      content={<div>asdfasdf</div>}
+      content={
+        <div className="flex">
+          {topMenuElements.map((el, i) => {
+            return <List key={i} title={el.title} elements={el.elements} />;
+          })}
+        </div>
+      }
       position="bottom"
       bodyProps={{
-        className: "bottom-[-61px]",
+        className: "p-6 translate-y-[0%]",
       }}
     >
       <div className="navEl text-nav">
---
+++ src/components/layout/mainLayout/Header/components/TopMenu/store/topMenuElements.conf.ts
@@ -0,0 +1,117 @@
+export const topMenuElements = [
+  {
+    title: "Products",
+    elements: [
+      {
+        label: "wallet",
+        to: "/wallet",
+      },
+      {
+        label: "Exchange",
+        to: "/exchange",
+      },
+      {
+        label: "Explorer",
+        to: "/explorer",
+      },
+      {
+        label: "Pay",
+        to: "/pay",
+      },
+      {
+        label: "Institutional",
+        to: "/institutional",
+      },
+      {
+        label: "Earn",
+        to: "/earn",
+      },
+      {
+        label: "Card",
+        to: "/card",
+      },
+      {
+        label: "Learn",
+        to: "/learn",
+      },
+      {
+        label: "Prices",
+        to: "/prices",
+      },
+      {
+        label: "Charts",
+        to: "/charts",
+      },
+      {
+        label: "NFT",
+        to: "/nft",
+      },
+    ],
+  },
+  {
+    title: "Resources",
+    elements: [
+      {
+        label: "APIs",
+        to: "/apis",
+      },
+      {
+        label: "Status",
+        to: "/status",
+      },
+      {
+        label: "Open Source",
+        to: "/open-source",
+      },
+      {
+        label: "Research",
+        to: "/research",
+      },
+      {
+        label: "Legal & Privacy",
+        to: "/legal-privacy",
+      },
+      {
+        label: "Support",
+        to: "/support",
+      },
+      {
+        label: "Blog",
+        to: "/blog",
+      },
+      {
+        label: "Security",
+        to: "/security",
+      },
+      {
+        label: "Podcast",
+        to: "/podcast",
+      },
+    ],
+  },
+  {
+    title: "Company",
+    elements: [
+      {
+        label: "About",
+        to: "/about",
+      },
+      {
+        label: "Careers",
+        to: "/careers",
+      },
+      {
+        label: "Press Center",
+        to: "/press-center",
+      },
+      {
+        label: "Prime",
+        to: "/prime",
+      },
+      {
+        label: "Ventures",
+        to: "/ventures",
+      },
+    ],
+  },
+];
