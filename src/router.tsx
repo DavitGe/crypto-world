e5768a0c765d8c42c34b455e13fddcf5342c7c46
Davit_Gelovani <65369123+DavitGe@users.noreply.github.com>
2024-07-02 23:29:52 +0400

    #1 - Main page changed

--- src/router.tsx
+++ src/router.tsx
@@ -1,20 +1,11 @@
-import { Navigate, createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/home";
-import WalletPage from "./pages/wallet/WalletPage";
+import LandingPage from "./pages/landing/LandingPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Navigate to={"home/wallet"} />,
-  },
-  {
-    path: "/home",
-    children: [
-      {
-        path: "wallet",
-        element: <Home children={<WalletPage />} />,
-      },
-    ],
+    element: <Home children={<LandingPage />} />,
   },
 ]);
 
